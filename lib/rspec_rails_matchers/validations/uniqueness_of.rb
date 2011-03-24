a1d6504dfe5d954bcc62bbe1f9516f4f92294de5
Vitaliy V. Shopov <unitymind.apps@gmail.com>
2011-03-25 05:33:14 +0700

    Rewrite validate_uniqueness_of matcher for unknown reason :) Uses monkey-patched Hash class with added deep_include? instance method.

--- lib/rspec_rails_matchers/validations/uniqueness_of.rb
+++ lib/rspec_rails_matchers/validations/uniqueness_of.rb
@@ -4,18 +4,8 @@ module RspecRailsMatchers
       def validate_uniqueness_of(attribute, options = {})
         Rspec::Matchers::Matcher.new :validate_uniqueness_of, attribute do |_attr_|
           match do |model|
-            duplicate = create_duplicate_record(model, _attr_, options[:scope])
-
-            if options[:scope]
-              model.send("#{options[:scope]}=", duplicate.send(options[:scope]))
-            end
-            
-            model.send("#{_attr_}=", "foobar")
-
-            model.invalid? && 
-              model.errors[_attr_].include?(
-                I18n::t('activerecord.errors.messages.taken')
-              )
+            model.class.validators.detect(Proc.new {false}) { |v| v.to_s.demodulize =~ /^UniquenessValidator/ &&
+                v.attributes.include?(_attr_) && v.options.deep_include?(options) }
           end
 
           failure_message_for_should do |model|
@@ -23,13 +13,6 @@ module RspecRailsMatchers
               :expected => 
                 [ "%s to validate uniqueness of %s, %s", model, _attr_, options ]
             )
-          end
-          
-          def create_duplicate_record( model, attr, scope )
-            m = model.class.new(attr => 'foobar')
-            m.send("#{scope}=", 11) if scope
-            m.save(:validate => false)
-            m
           end
         end
       end
