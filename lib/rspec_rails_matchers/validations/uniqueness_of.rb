24cc6a1bd8963ba5797f46b913be7e0797de7a7c
Vitaliy V. Shopov <unitymind.apps@gmail.com>
2011-03-25 07:08:45 +0700

    Previous unknown reason detected :) Rails 3 validate_uniqueness_of  has some options key, like such the :scope, :case_sensitive, :allow_nil and more. If we are no have this options in validate_uniqueness_of matcher, but not have in the matched model - this is different validations and not should be match. Used model.class.validators method for find matched ActiveRecord validation, no model.send approach.

--- lib/rspec_rails_matchers/validations/uniqueness_of.rb
+++ lib/rspec_rails_matchers/validations/uniqueness_of.rb
@@ -3,15 +3,19 @@ module RspecRailsMatchers
     module UniquenessOf
       def validate_uniqueness_of(attribute, options = {})
         Rspec::Matchers::Matcher.new :validate_uniqueness_of, attribute do |_attr_|
+          options[:case_sensitive] = true if options[:case_sensitive].nil?
+          actual_options = {}
           match do |model|
-            model.class.validators.detect(Proc.new {false}) { |v| v.to_s.demodulize =~ /^UniquenessValidator/ &&
-                v.attributes.include?(_attr_) && v.options.deep_include?(options) }
+            if validator = model.class.validators.find(Proc.new {false}) { |v| v.to_s.demodulize =~ /^UniquenessValidator/ && v.attributes.include?(_attr_) }
+              actual_options = validator.options
+              validator.options.deep_include?(options) && validator.options.keys.sort == options.keys.sort
+            end
           end
 
           failure_message_for_should do |model|
             RspecRailsMatchers::Message.error(
-              :expected => 
-                [ "%s to validate uniqueness of %s, %s", model, _attr_, options ]
+              :expected => [ "%s to validate uniqueness of %s, %s", model, _attr_, options ],
+              :actual   => [ "%s to validate uniqueness of %s, %s", model, _attr_, actual_options]
             )
           end
         end
